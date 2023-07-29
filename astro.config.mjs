e5ca03f2fb604bb924f51d44cdd0f1b5a6395acb
Sanshiroh SAKAI <sansyo@gmail.com>
2023-07-30 07:59:47 +0900

    Tailwind 追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
 import mdx from '@astrojs/mdx'
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), mdx()],
+  integrations: [react(), mdx(), tailwind()],
 })
