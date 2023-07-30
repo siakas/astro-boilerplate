c9ac330c13f42fe5f8dd088359765ede857ed170
Sanshiroh SAKAI <sansyo@gmail.com>
2023-07-31 05:46:05 +0900

    ブログ追加

---
+++ src/components/Card.tsx
@@ -0,0 +1,29 @@
+import styles from './Card.module.scss'
+
+type CardProps = {
+  id: number
+  title: string
+}
+
+const Card = ({ id, title }: CardProps) => {
+  return (
+    <a
+      href={`/blog/${title.replaceAll(' ', '-').toLowerCase()}`}
+      className={styles.card}
+    >
+      <img
+        src={`https://picsum.photos/id/${id * 4}/600/600`}
+        alt=""
+        loading="lazy"
+        className={styles.image}
+      />
+      <div className={styles.bg}></div>
+      <div className={styles.text}>
+        <p className={styles.title}>{title}</p>
+        <p className={styles.more}>続きを読む</p>
+      </div>
+    </a>
+  )
+}
+
+export default Card
