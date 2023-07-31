fb08f31188f29def94ee2d49d4d4cca3c9d86595
Sanshiroh SAKAI <sakai.s@ekzm.co.jp>
2023-07-31 11:00:17 +0900

    出力設定を変更

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,4 +9,25 @@ export default defineConfig({
     open: true,
   },
   integrations: [react(), mdx(), compress()],
+  vite: {
+    build: {
+      rollupOptions: {
+        output: {
+          assetFileNames: (assetInfo) => {
+            let extType = assetInfo.name.split('.').at(-1)
+            if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+              extType = 'img'
+            }
+            if (/ts|js/i.test(extType)) {
+              extType = 'script'
+            }
+            if (/css|scss/i.test(extType)) {
+              extType = 'styles'
+            }
+            return `assets/${extType}/[name]-[hash][extname]`
+          },
+        },
+      },
+    },
+  },
 })
